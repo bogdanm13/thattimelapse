5a454dbc4a340fffde70bf61c7a93d2ad1a8a13c
bogdanm13 <bmarincas@gmail.com>
2017-06-14 06:49:42 +0300

    capturing one image

--- timelapse.py
+++ timelapse.py
@@ -1,9 +1,25 @@
 import sys
+from time import sleep
+
+from picamera import PiCamera
+
+
+class TimeLapser(object):
+
+    def __init__(self, camera):
+        self.camera = camera
+
+    def capture_image(self, path):
+        self.camera.start_preview()
+        sleep(5)
+        self.camera.capture(path)
+        self.camera.stop_preview()
 
 
 def main():
-    pass
-
+    camera = PiCamera()
+    tl = TimeLapser(camera)
+    tl.capture_image('/home/pi/Desktop/capture.jpg')
 
 if __name__ == "__main__":
     sys.exit(main())
