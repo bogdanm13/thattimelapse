ea6bd447438418942b83ad472c2a8ea0ba8480f4
bogdanm13 <bmarincas@gmail.com>
2017-06-15 06:24:33 +0300

    and actually using it

--- timelapse.py
+++ timelapse.py
@@ -10,20 +10,21 @@ class TimeLapser(PiCamera):
 
     def __init__(self, prefix, folder, wait, count=10, config={}):
         self.prefix = prefix
-        self.folder = folder
+        self.folder = os.path.abspath(folder)
         self.wait = wait
         self.count = count
         self.camera_config = config
         PiCamera.__init__(self)
 
     def prepare(self):
-        os.makedirs(os.path.abspath(self.folder))
-        
+        os.makedirs(self.folder)
+
     def configure(self):
         # doing this after init since some of them do not work
         self.rotation = self.camera_config['rotation'] or 0
 
     def lapse(self):
+        self.prepare()
         self.configure()
         self.start_preview()
         sleep(WAIT_INIT)
